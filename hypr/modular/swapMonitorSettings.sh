d4ae343f42ac36f40742a7cc3d52d0172a79bb7a
0schult <philipp.schult@studium.uni-hamburg.de>
2024-10-19 13:00:30 +0200

    Add modular hyprland config

---
+++ hypr/modular/swapMonitorSettings.sh
@@ -0,0 +1,3 @@
+mv ~/.config/hypr/modular/monitors.conf ~/.config/hypr/modular/monitors.conf.temp
+mv ~/.config/hypr/modular/monitors.conf.bak ~/.config/hypr/modular/monitors.conf
+mv ~/.config/hypr/modular/monitors.conf.temp ~/.config/hypr/modular/monitors.conf.bak
